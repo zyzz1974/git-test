cae975faf270147704d8ecddc099da0389e96f00
zyzz <zyzz@qq.com>
2022-02-09 20:47:40 +0800

    second commit

--- src/main/java/com/git/test/GitTest.java
+++ src/main/java/com/git/test/GitTest.java
@@ -3,5 +3,6 @@ package com.git.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test!");
+        System.out.println("git test2!");
     }
 }
