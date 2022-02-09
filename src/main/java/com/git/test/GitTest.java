9a4e1b2b4bd867b713b77118f425b5167cc22e20
zyzz <zyzz@qq.com>
2022-02-09 21:09:09 +0800

    hot fix commit3

--- src/main/java/com/git/test/GitTest.java
+++ src/main/java/com/git/test/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("git test!");
         System.out.println("git test2!");
         System.out.println("git test3!");
-        System.out.println("hot fixes");
+        System.out.println("hot fixes test");
     }
 }
